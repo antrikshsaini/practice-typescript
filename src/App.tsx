cd9f67bac48f56e0175e375b565796770170110b
Antrix <sainiantriksh6@gmail.com>
2021-01-26 14:10:41 -0800

    learned event handlers and refs

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import GuestList from './state/GuestList';
-import UserSearch from './state/UserSearch';
+import UserSearch from './refs/UserSearch';
+import EventComponent from './events/EventComponent';
 
 function App() {
   return (
     <div className="App">
       <GuestList />
       <UserSearch />
+      <EventComponent />
     </div>
   );
 }
